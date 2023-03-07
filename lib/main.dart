5e8c08146a2dbb6be76c31c42302c0cb481fad62
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-07 22:37:26 +0330

    bug of not sending requests in release fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:requester/config/app_theme.dart';
 import 'package:requester/config/languages/en.dart';
@@ -8,6 +10,7 @@ import 'features/feature_requester/presentation/pages/home_page.dart';
 Future<void> main() async {
   ///  init locator
   await setupDI();
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -22,5 +25,14 @@ class MyApp extends StatelessWidget {
       theme: appThemeData(),
       home: const HomePage(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
