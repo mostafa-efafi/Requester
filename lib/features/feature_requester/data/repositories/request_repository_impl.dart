1cd3cd4fe782f9ee6c573623ea1d0533182fbdec
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-08 10:17:19 +0330

    fixed repositories for string result

--- lib/features/feature_requester/data/repositories/request_repository_impl.dart
+++ lib/features/feature_requester/data/repositories/request_repository_impl.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:requester/core/resource/data_state.dart';
+import 'package:requester/core/utils/constants.dart';
 import 'package:requester/features/feature_requester/data/data_source/remote/api_provider.dart';
 import 'package:requester/features/feature_requester/domain/repositories/request_repository.dart';
 
@@ -10,8 +13,14 @@ class RequestRepositoryImpl implements RequestRepository {
   Future<DataState<Map<String, dynamic>>> fetchGetMethod(String url) async {
     final response = await apiProvider.getMethod(url);
     if (response is DataSuccess && response.data != null) {
-      final data = response.data!.data;
-      return DataSuccess(data);
+      try {
+        final toMap = response.data!.data is String
+            ? json.decode(response.data!.data as String)
+            : response.data;
+        return DataSuccess(toMap);
+      } catch (e) {
+        return const DataFailed(Constants.noOutputExist);
+      }
     } else {
       return DataFailed(response.error!);
     }
@@ -19,11 +28,17 @@ class RequestRepositoryImpl implements RequestRepository {
 
   @override
   Future<DataState<Map<String, dynamic>>> fetchPostMethod(
-      {required String url, Map<String , dynamic>? body}) async {
+      {required String url, Map<String, dynamic>? body}) async {
     final response = await apiProvider.postMethod(url: url, body: body);
     if (response is DataSuccess && response.data != null) {
-      final data = response.data!.data;
-      return DataSuccess(data);
+      try {
+        final toMap = response.data!.data is String
+            ? json.decode(response.data!.data as String)
+            : response.data;
+        return DataSuccess(toMap);
+      } catch (e) {
+        return const DataFailed(Constants.noOutputExist);
+      }
     } else {
       return DataFailed(response.error!);
     }
