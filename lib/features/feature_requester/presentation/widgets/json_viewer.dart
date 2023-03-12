bfcc0640e3e58134a8763e325843797e367951e7
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-12 21:12:33 +0330

    updated jsonViewer widget

--- lib/features/feature_requester/presentation/widgets/json_viewer.dart
+++ lib/features/feature_requester/presentation/widgets/json_viewer.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_json_view/flutter_json_view.dart';
-import 'package:requester/core/utils/constants.dart';
 import 'package:requester/features/feature_requester/presentation/bloc/home_page_bloc/home_page_bloc.dart';
 import 'package:requester/features/feature_requester/presentation/bloc/home_page_bloc/request_status.dart';
 
@@ -17,10 +18,10 @@ class JsonViewer extends StatelessWidget {
         if (state.requestStatus is RequestLoaded) {
           /// [Loaded] response
           final data = state.requestStatus as RequestLoaded;
-          return _jsonVContainer(data.response);
+          return _jsonVContainer(data.response.data!);
         } else if (state.requestStatus is RequestNoAction) {
           /// for sample show json data
-          return _jsonVContainer(Constants.sampleJson);
+          return const SizedBox();
         } else if (state.requestStatus is RequestLoading) {
           return Column(
             children: const [
@@ -34,20 +35,27 @@ class JsonViewer extends StatelessWidget {
     );
   }
 
-  JsonView _jsonVContainer(Map<String, dynamic> jsonData) {
-    return JsonView.map(
-      jsonData,
-      theme: JsonViewTheme(
-        backgroundColor: Colors.grey.shade100,
-        closeIcon:
-            const Icon(Icons.arrow_drop_up, size: 18, color: Colors.black),
-        openIcon:
-            const Icon(Icons.arrow_drop_down, size: 18, color: Colors.black),
-        stringStyle: TextStyle(
-          color: Colors.orange[600],
-          fontSize: 16,
+  Widget _jsonVContainer(String data) {
+    final toJson = json.decode(data);
+    if (toJson is Map<String, dynamic>) {
+      return JsonView.map(
+        toJson,
+        theme: JsonViewTheme(
+          backgroundColor: Colors.grey.shade100,
+          closeIcon:
+              const Icon(Icons.arrow_drop_up, size: 18, color: Colors.black),
+          openIcon:
+              const Icon(Icons.arrow_drop_down, size: 18, color: Colors.black),
+          stringStyle: TextStyle(
+            color: Colors.orange[600],
+            fontSize: 16,
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return const SizedBox();
+
+      /// replace with [WebView]
+    }
   }
 }
