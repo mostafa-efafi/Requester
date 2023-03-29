da2c00c28245619b22eac1a5b09fe42cbc269b75
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-29 09:37:17 +0330

    updated NetworkChecker class test

--- test/core/rest/network_checker_test.dart
+++ test/core/rest/network_checker_test.dart
@@ -1,37 +1,27 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:requester/core/rest/network_checker.dart';
 import 'package:requester/di.dart';
 
-import 'network_checker_test.mocks.dart';
-
-@GenerateMocks([Connectivity])
-late MockConnectivity mockConnectivity;
-
 void main() {
   group('checkConnection method', () {
-    mockConnectivity = MockConnectivity();
     final networkChecker = di<NetworkChecker>();
-    test('Should return true when mobile data is on', () async {
-      // arrange
-      when(mockConnectivity.checkConnectivity())
-          .thenAnswer((realInvocation) async => ConnectivityResult.mobile);
-      // act
-      final result = await networkChecker.checkConnection();
-      // assert
-      expect(result, true);
+    test('When internet connection is available', () async {
+      //arrange
+      bool expected = true;
+      //act
+      bool actual = await networkChecker.checkConnection();
+      //assert
+      expect(actual, expected);
     });
 
-    test('Should return true when wifi is on', () async {
-      // arrange
-      when(mockConnectivity.checkConnectivity())
-          .thenAnswer((realInvocation) async => ConnectivityResult.wifi);
-      // act
-      final result = await networkChecker.checkConnection();
-      // assert
-      expect(result, true);
+    test('When internet connection is not available', () async {
+      /// turn off [wifi] and [data]
+      //arrange
+      bool expected = false;
+      //act
+      bool actual = await networkChecker.checkConnection();
+      //assert
+      expect(actual, expected);
     });
   });
 }
