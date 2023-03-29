9f20c4656ca4de943b105dd85e01a69fabaa1e77
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-29 09:36:47 +0330

    updated NetworkChecker class

--- lib/core/rest/network_checker.dart
+++ lib/core/rest/network_checker.dart
@@ -1,15 +1,17 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io';
 
 /// Return [true] if the device was connected to the Internet, otherwise [false]
 class NetworkChecker {
+  static const urlTest = 'google.com';
   Future<bool> checkConnection() async {
-    if (kIsWeb) {
-      return true;
-    } else {
-      var connectivityResult = await (Connectivity().checkConnectivity());
-      return connectivityResult == ConnectivityResult.mobile ||
-          connectivityResult == ConnectivityResult.wifi;
+    try {
+      final result = await InternetAddress.lookup(urlTest);
+      if (result.isNotEmpty && result.first.rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      return false;
     }
+    return false;
   }
 }
