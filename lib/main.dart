cdaade1a6d244bbc4e42c82c0d05a1fd6ac3361d
Mostafa Efafi <mostafa.ef@gmail.com>
2024-06-26 11:44:23 +0330

    added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:requester/config/app_theme.dart';
 import 'package:requester/config/languages/en.dart';
 import 'package:requester/di.dart';
+import 'package:requester/firebase_options.dart';
 
 import 'features/feature_requester/presentation/pages/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// init Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   ///  init locator
   await setupDI();
   HttpOverrides.global = MyHttpOverrides();
